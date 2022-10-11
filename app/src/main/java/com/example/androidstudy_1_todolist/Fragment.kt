9ed9bce5ad441bc4034b6e7ea1984b8a7663201a
HyunDDeung <gustmd417@gmail.com>
2022-10-11 19:29:23 +0900

    study

---
+++ app/src/main/java/com/example/androidstudy_1_todolist/Fragment.kt
@@ -0,0 +1,30 @@
+package com.example.androidstudy_1_todolist
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [Fragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class Fragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_, container, false)
+
+
+
+
+
+    }
+
+}
