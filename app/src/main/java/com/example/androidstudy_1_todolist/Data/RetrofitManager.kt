feb90de163af57c97a2077ac5a6cf4d10b1d5524
HyunDDeung <gustmd417@gmail.com>
2022-12-31 23:48:19 +0900

    Feat:"구현 - retrofit 객체 초기화"

---
+++ app/src/main/java/com/example/androidstudy_1_todolist/Data/RetrofitManager.kt
@@ -0,0 +1,15 @@
+package com.example.androidstudy_1_todolist.Data
+
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+/** 레트로핏 객체 초기화 */
+
+object RetrofitManager {
+    private val BASE_URL = "https://todolist-369816.df.r.appspot.com"
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl(BASE_URL)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+}
