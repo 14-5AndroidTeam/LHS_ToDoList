97f02e1a4ba158f2ac0486a49ff777e85c448868
HyunDDeung <gustmd417@gmail.com>
2023-01-02 17:12:31 +0900

    Refactor:"변경 - api 키값 숨김"

--- app/src/main/java/com/example/androidstudy_1_todolist/Data/RetrofitManager.kt
+++ app/src/main/java/com/example/androidstudy_1_todolist/Data/RetrofitManager.kt
@@ -1,12 +1,13 @@
 package com.example.androidstudy_1_todolist.Data
 
+import com.example.androidstudy_1_todolist.BuildConfig
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 /** 레트로핏 객체 초기화 */
 
 object RetrofitManager {
-    private val BASE_URL = "https://todolist-369816.df.r.appspot.com"
+    private val BASE_URL = BuildConfig.SERVER
 
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
