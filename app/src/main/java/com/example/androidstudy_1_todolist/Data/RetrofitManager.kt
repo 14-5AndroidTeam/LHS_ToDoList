cb8e3cf6aebe610f277f2271c4b4774ecd9eaf31
HyunDDeung <gustmd417@gmail.com>
2023-01-03 02:17:37 +0900

    Fix:"수정 - api 실행 오류 해결"

--- app/src/main/java/com/example/androidstudy_1_todolist/Data/RetrofitManager.kt
+++ app/src/main/java/com/example/androidstudy_1_todolist/Data/RetrofitManager.kt
@@ -7,7 +7,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 /** 레트로핏 객체 초기화 */
 
 object RetrofitManager {
-    private val BASE_URL = BuildConfig.SERVER
+    private val BASE_URL = "https://todolist-369816.df.r.appspot.com"
 
     val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
