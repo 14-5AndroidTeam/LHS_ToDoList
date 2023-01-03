a238088c70827719c08ade50c872114ab9507950
HyunDDeung <gustmd417@gmail.com>
2023-01-03 16:03:24 +0900

    Feat:"구현 - todo리스트 수정 기능"

--- app/src/main/java/com/example/androidstudy_1_todolist/ModifyFragment.kt
+++ app/src/main/java/com/example/androidstudy_1_todolist/ModifyFragment.kt
@@ -5,55 +5,79 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.setFragmentResultListener
+import androidx.lifecycle.Observer
+import com.example.androidstudy_1_todolist.UI.View.ListFragment
+import com.example.androidstudy_1_todolist.UI.ViewModel.ListModel
+import com.example.androidstudy_1_todolist.databinding.FragmentModifyBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ModifyFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ModifyFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding: FragmentModifyBinding
+    var viewmodel = ListModel()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_modify, container, false)
+        binding = FragmentModifyBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ModifyFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ModifyFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        var itemId:String
+
+        /** ListFragment에서 pos값과 해당하는 todo값을 전달받았다. */
+        setFragmentResultListener("requestKey") { key, bundle ->
+            itemId = bundle.getString("id").toString()
+            var itemContent = bundle.getString("content").toString()
+            var itemDeadline = bundle.getString("deadline").toString()
+
+            /** 전달받은 값을 화면의 content와 deadline에 띄워준다. */
+            binding.textContent.setText(itemContent)
+            binding.textDeadline.setText(itemDeadline)
+        }
+
+        /** cancel 버튼 이벤트 처리*/
+        binding.btnCancel.setOnClickListener {
+            parentFragmentManager
+                .beginTransaction()
+                .replace(R.id.layout, ListFragment())
+                .commit()
+        }
+
+        /** submit 이벤트 처리 */
+        binding.btnSubmit.setOnClickListener {
+            /**공백데이터가 아닌 경우만 서버로 제출을 허용함*/
+            if(binding.textContent.text.isEmpty() or binding.textDeadline.text.isEmpty()){
+                /** 공백데이터인 경우 ToastMessage 출력 */
+                Toast.makeText(getActivity(), "공백데이터 존재", Toast.LENGTH_SHORT).show()
             }
+            else{
+                viewmodel.putList(
+                    // 선언 에러 나는 이유,,?
+                    // 일단 임시 아이디 값 지정
+                    1,
+                    binding.textContent.text.toString(),
+                    binding.textDeadline.text.toString())
+            }
+            /** 뷰모델에게 할일,기한 데이터를 뽑아서 전달한다 */
+
+            /** 관찰하고 있는 뷰모델의 LiveData의 Observer */
+            viewmodel.put.observe(viewLifecycleOwner, Observer {
+                /**서버로 보낸 제출요청에 대한 응답이 성공인 것으로 확인됨.
+                 * 따라서 메인화면으로 이동*/
+
+                /** 수정 성공에 대한 토스트 메시지 띄우기 */
+                Toast.makeText(getActivity(), "수정 성공!", Toast.LENGTH_SHORT).show()
+
+                parentFragmentManager
+                    .beginTransaction()
+                    .replace(R.id.layout, ListFragment())
+                    .commit()
+            })
+        }
     }
 }
